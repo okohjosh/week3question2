0f0dded53d2ca3205efa73f1bc79b893deaa5f58
okohjosh <134227165+okohjosh@users.noreply.github.com>
2023-05-27 08:42:34 +0100

    Create joseph.php

---
+++ joseph.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = "Okoh Joseph Onah";
+$regnumber = "2021/HND/34353/CS";
+$soo = "Cross River State";
+$topic = "design and developmen of students web base advising system A case of akanu-ibiam federal polytechnic unwana";
+
+
+echo "Name: $name<br>";
+echo "Regnumber: $regnumber<br>";
+echo "State of origin: $soo<br>";
+echo "Project topic: $topic<br>";
+
+?>
